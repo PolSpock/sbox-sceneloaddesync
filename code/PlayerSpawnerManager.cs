e9082529ba5c800e5f9677b8a94d88c6756bce76
PolSpock <5229571+PolSpock@users.noreply.github.com>
2024-06-11 22:07:19 +0200

    UseCollisionRules

--- code/PlayerSpawnerManager.cs
+++ code/PlayerSpawnerManager.cs
@@ -27,6 +27,7 @@ public sealed class PlayerSpawnerManager : Component, Component.INetworkListener
 		eyes.SetParent( player );
 
 		var characterController = player.Components.Create<CharacterController>();
+		characterController.UseCollisionRules = true;
 		var playerController = player.Components.Create<PlayerController>();
 		playerController.Body = body;
 		playerController.Eye = eyes;
